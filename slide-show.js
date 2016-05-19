556050cd5fb9544645d869561e062295d940cfa6
Thomas Wilburn <news@thomaswilburn.net>
2016-05-18 20:06:11 -0700

    Add code slides Make code-slide and text-slide their own custom elements Some edits based on a run-through

--- slide-show.js
+++ slide-show.js
@@ -1,12 +1,51 @@
-//Introducing: <slide-show>!
+// Introducing: <slide-show> (and its partner, <text-slide>)!
 
 // Throughout this text, "V1" refers to the new customElements interface:
 // https://w3c.github.io/webcomponents/spec/custom/
 
-// Styles for the element's children
+// First, as a warm-up, we'll make text-slide work:
+
+var textSlideProto = Object.create(HTMLElement.prototype);
+// In V1 and ES6, this line is way easier: 
+// class TextSlideElement extends HTMLElement { ...
+
+// Parse the world's worst structured text format
+textSlideProto.parseContents = function() {
+  var lines = this.innerHTML.trim().split("\n");
+  // First line is a headline
+  var headline = lines.shift();
+  var body = lines.map(function(line) {
+    line = line.trim();
+    // Blank lines become paragraphs
+    if (!line) return "<p>";
+    // Terrible bold/preformatted support
+    return line.replace(/\*(.*?)\*/g, "<b>$1</b>") + "<br>";
+  }).join("\n");
+  return { headline, body }; 
+};
+
+// Register the element
+document.registerElement("text-slide", { prototype: textSlideProto });
+// V1 version:
+// window.customElements.define("text-slide", TextSlideElement);
+
+// Let's also make a code-slide, which will be very similar
+var codeSlideProto = Object.create(HTMLElement.prototype);
+codeSlideProto.parseContents = function() {
+  var lines = this.innerHTML.trim().split("\n");
+  var headline = lines.shift();
+  var minLeading = Math.min.apply(null, lines.filter(l => l.trim()).map(l => l.match(/^\s*/)[0].length));
+  var replacer = new RegExp(`^\\s{${minLeading}}`);
+  var body = lines.map(l => l.replace(replacer, "")).join("\n").trim();
+  body = "<pre><code>" + body + "</code></pre>";
+  return { headline, body }
+};
+document.registerElement("code-slide", { prototype: codeSlideProto });
+
+// Now for the slide-show itself, which will use more advanced features:
+
+// Styles for the slide-show element's children (which we will generate)
 var stylesheet = `
-  text-slide { display: none }
-  
   .content {
     font-family: Roboto, sans-serif;
     font-size: 36px;
@@ -39,20 +78,32 @@ var stylesheet = `
   
   .content a:visited {
     color: #333;
-  }`;
+  }
+  
+  .content pre {
+    font-size: 50%;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-align: left;
+    padding: 20px;
+    box-shadow: inset 4px 4px 16px rgba(255, 255, 255, .1);
+    background: #333;
+    color: #eee
+  }
+  `;
 
-var proto = Object.create(HTMLDivElement.prototype);
-// In V1 and ES6, this line is way easier: 
-// class SlideShowElement extends HTMLElement { ...
+// Create its prototype
+var slideShowProto = Object.create(HTMLDivElement.prototype);
 
 // Called when the element is first created
 // V1: this is done in the constructor
-proto.createdCallback = function() {
+slideShowProto.createdCallback = function() {
+  
   // If we have Shadow DOM, let's make a root to put our generated HTML
   // Note: createShadowRoot() is becoming attachShadow()
   var root = this.createShadowRoot ? this.createShadowRoot() : this;
   
-  // Add styles
+  // Add styles (scoped, similar to iframe, if Shadow DOM is supported)
   var style = document.createElement("style");
   style.innerHTML = stylesheet;
   root.appendChild(style);
@@ -83,7 +134,7 @@ proto.createdCallback = function() {
 
 // When attributes change, update the slideshow to match
 // In V1, you must specify `observedAttributes` manually
-proto.attributeChangedCallback = function(prop, before, after) {
+slideShowProto.attributeChangedCallback = function(prop, before, after) {
   switch (prop) {
     case "index":
       this.setSlide(after);
@@ -94,30 +145,34 @@ proto.attributeChangedCallback = function(prop, before, after) {
 };
 
 // V1: this is "connectedCallback" instead
-proto.attachedCallback = function() {
+slideShowProto.attachedCallback = function() {
   // Hook up our observer whenever we're in the document
-  this.state.observer.observe(this, { subtree: true, childList: true, characterData: true });
+  this.state.observer.observe(this, {
+    subtree: true,
+    childList: true,
+    characterData: true
+  });
   this.setSlide(this.state.current);
 };
 
 // V1: this becomes "disconnectedCallback"
-proto.detachedCallback = function() {
+slideShowProto.detachedCallback = function() {
   // Don't watch for changes when not connected
   this.state.observer.disconnect();
 };
 
 // These methods are available on the element itself
-proto.setSlide = function(index) {
+slideShowProto.setSlide = function(index) {
   index *= 1;
-  // Find all <text-slide> children, and grab the current slide
-  var items = this.querySelectorAll("text-slide");
+  // Find all slide children, and grab the current slide
+  var items = this.querySelectorAll("text-slide, code-slide");
   var selected = items[index];
   if (!selected) return;
   // Update our internal state
   this.state.current = index;
   this.state.length = items.length;
-  // Load and transform <text-slide>'s contents
-  var slide = parseSlideText(selected.innerHTML);
+  // Load the slide's contents
+  var slide = selected.parseContents();
   // Fill the content div with our new slide contents
   this.state.content.innerHTML = `<h1>${slide.headline}</h1> ${slide.body}`;
   // Let listeners know that we've changed slides
@@ -130,37 +185,21 @@ proto.setSlide = function(index) {
 };
 
 //boring convenience methods
-proto.getSlide = function() {
+slideShowProto.getSlide = function() {
   return this.state.current;
 };
 
-proto.shiftSlide = function(delta) {
+slideShowProto.shiftSlide = function(delta) {
   this.setSlide(this.state.current + delta);
 };
 
-proto.nextSlide = function() {
+slideShowProto.nextSlide = function() {
   this.shiftSlide(1);
 };
 
-proto.previousSlide = function() {
+slideShowProto.previousSlide = function() {
   this.shiftSlide(-1);
 };
 
-// This is the world's worst structured text format
-var parseSlideText = function(contents) {
-  contents = contents.trim().split("\n");
-  // First line is a headline
-  var headline = contents.shift();
-  var body = contents.map(function(line) {
-    line = line.trim();
-    // Blank lines become paragraphs
-    if (!line) return "<p>";
-    // Terrible bold/preformatted support
-    return line.replace(/\*(.*?)\*/g, "<b>$1</b>") + "<br>";
-  }).join("\n");
-  return { headline, body }; 
-};
-
-document.registerElement("slide-show", { prototype: proto });
-// V1 version:
-// window.customElements.define("slide-show", SlideShowElement);
+// And now, the final registration:
+document.registerElement("slide-show", { prototype: slideShowProto });
