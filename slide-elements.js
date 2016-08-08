bfefdd5f1a0edf74f8cda04e5cac3d71672f3d00
Thomas Wilburn <news@thomaswilburn.net>
2016-08-08 16:33:59 -0700

    Update to use the class syntax

--- slide-elements.js
+++ slide-elements.js
@@ -5,55 +5,68 @@
 var textProto = Object.create(HTMLElement.prototype);
 var codeProto = Object.create(textProto);
 
-textProto.createdCallback = function() {};
+class TextSlideElement extends HTMLElement {
 
-textProto.notifyParent = function() {
-  var e = new CustomEvent("slide-content", { bubbles: true });
-  this.parseSlide();
-  this.dispatchEvent(e);
-};
-
-textProto.attachedCallback = function() {
-  if (!this.observer_) {
-    this.observer_ = new MutationObserver(() => this.notifyParent());
+  constructor() {
+    super();
+    this.connectedCallback = this.attachedCallback;
+    this.disconnectedCallback = this.detachedCallback;
   }
-  this.observer_.observe(this, { characterData: true, childList: true, subtree: true });
-  this.notifyParent();
-}
 
-textProto.detachedCallback = function() {
-  if (this.observer_) {
-    this.observer_.disconnect();
+  notifyParent() {
+    var e = new CustomEvent("slide-content", { bubbles: true });
+    this.parseSlide();
+    this.dispatchEvent(e);
+  };
+
+  attachedCallback() {
+    if (!this.observer_) {
+      this.observer_ = new MutationObserver(() => this.notifyParent());
+    }
+    this.observer_.observe(this, { characterData: true, childList: true, subtree: true });
+    this.notifyParent();
   }
-}
 
-textProto.parseSlide = function() {
-  var html = this.innerHTML;
-  var lines = html.trim().split("\n");
-  // First line is a headline
-  var headline = lines.shift().replace(/\*(.*?)\*/g, "<b>$1</b>");
-  var body = lines.map(function(line) {
-    line = line.trim();
-    // Blank lines become paragraphs
-    if (!line) return "<p>";
-    // Terrible bold/preformatted support
-    return line.replace(/\*(.*?)\*/g, "<b>$1</b>") + "<br>";
-  }).join("\n");
-  this.parsedContent = { headline, body };
-  return { headline, body };
+  detachedCallback() {
+    if (this.observer_) {
+      this.observer_.disconnect();
+    }
+  }
+
+  parseSlide() {
+    var html = this.innerHTML;
+    var lines = html.trim().split("\n");
+    // First line is a headline
+    var headline = lines.shift().replace(/\*(.*?)\*/g, "<b>$1</b>");
+    var body = lines.map(function(line) {
+      line = line.trim();
+      // Blank lines become paragraphs
+      if (!line) return "<p>";
+      // Terrible bold/preformatted support
+      return line.replace(/\*(.*?)\*/g, "<b>$1</b>") + "<br>";
+    }).join("\n");
+    this.parsedContent = { headline, body };
+    return { headline, body };
+  }
 };
 
-codeProto.parseSlide = function() {
-  var html = this.innerHTML;
-  var lines = html.trim().split("\n");
-  var headline = lines.shift();
-  var minLeading = Math.min.apply(null, lines.filter(l => l.trim()).map(l => l.match(/^\s*/)[0].length));
-  var replacer = new RegExp(`^\\s{${minLeading}}`);
-  var body = lines.map(l => l.replace(replacer, "")).join("\n").trim();
-  body = "<pre><code>" + body + "</code></pre>";
-  this.parsedContent = { headline, body };
-  return { headline, body }
+class CodeSlideElement extends TextSlideElement {
+  constructor() {
+    super();
+  }
+
+  parseSlide() {
+    var html = this.innerHTML;
+    var lines = html.trim().split("\n");
+    var headline = lines.shift();
+    var minLeading = Math.min.apply(null, lines.filter(l => l.trim()).map(l => l.match(/^\s*/)[0].length));
+    var replacer = new RegExp(`^\\s{${minLeading}}`);
+    var body = lines.map(l => l.replace(replacer, "")).join("\n").trim();
+    body = "<pre><code>" + body + "</code></pre>";
+    this.parsedContent = { headline, body };
+    return { headline, body }
+  }
 };
 
-document.registerElement("text-slide", { prototype: textProto });
-document.registerElement("code-slide", { prototype: codeProto });
+document.registerElement("text-slide", { prototype: TextSlideElement.prototype });
+document.registerElement("code-slide", { prototype: CodeSlideElement.prototype });
--- slide-show.js
+++ slide-show.js
@@ -59,151 +59,146 @@ var stylesheet = `
 // create the element constructor
 // This is only called directly in V1
 // V1 also recommends `class SlideShowElement extends HTMLElement`
-var SlideShowElement = function() {}
-
-// Create its prototype
-var slideShowProto = SlideShowElement.prototype = Object.create(HTMLElement.prototype);
-
-// Called when the element is first created
-// V1: this is done in the constructor
-slideShowProto.createdCallback = function() {};
-
-// By moving initialization into its own method, we can trigger it for
-// both V0 and V1 codepaths
-slideShowProto.upgrade = function() {
-  this.upgraded = true;
-
-  // If we have Shadow DOM, let's make a root to put our generated HTML
-  // Note: createShadowRoot() is becoming attachShadow()
-  var root = this.createShadowRoot ? this.createShadowRoot() : this;
-  
-  // Add styles (scoped, similar to iframe, if Shadow DOM is supported)
-  var style = document.createElement("style");
-  style.innerHTML = stylesheet;
-  root.appendChild(style);
-  
-  // Add a div to be the visible slide
-  var content = document.createElement("div");
-  content.className = "content";
-  content.setAttribute("aria-live", "assertive");
-  content.setAttribute("aria-atomic", true);
-  content.setAttribute("aria-relevant", "text");
-  root.appendChild(content);
-  
-  // deferred rendering
-  this.waitingToRender = null;
-  
-  // Let's make some state available and start up
-  this.index_ = this.getAttribute("index") * 1;
-  this.length = 0;
-  this.dom = { content, root };
-  this.scheduleRender();
-  this.dispatchEvent(new CustomEvent("slides-ready", {
-    detail: {
-      index: this.index_,
-      length: this.length
-    }
-  }));
-  
-  // Trigger attribute changes after construction
-  // In V1, they may not be available in the constructor,
-  // so this makes our code a bit more future-proof
-  for (var i = 0; i < this.attributes.length; i++) {
-    var attr = this.attributes[i];
-    this.attributeChangedCallback(attr.name, null, attr.value);
+class SlideShowElement extends HTMLElement {
+  constructor() {
+    super();
   }
-  
-  // Listen for updates from children
-  this.addEventListener("slide-content", e => this.scheduleRender());
-  var watcher = new MutationObserver(() => this.scheduleRender());
-  watcher.observe(this, { childList: true });
-};
 
-// When attributes change, update the slideshow to match
-// In V1, you must specify `observedAttributes` manually
-slideShowProto.attributeChangedCallback = function(prop, before, after) {
-  switch (prop) {
-    case "index":
-      this.scheduleRender(after);
-      break;
-      
-    // TODO: other attributes go here
+  // Called when the element is first created
+  // V1: this is done in the constructor
+  createdCallback() {};
+
+  // By moving initialization into its own method, we can trigger it for
+  // both V0 and V1 codepaths
+  upgrade() {
+    this.upgraded = true;
+
+    // If we have Shadow DOM, let's make a root to put our generated HTML
+    // Note: createShadowRoot() is becoming attachShadow()
+    var root = this.createShadowRoot ? this.createShadowRoot() : this;
+    
+    // Add styles (scoped, similar to iframe, if Shadow DOM is supported)
+    var style = document.createElement("style");
+    style.innerHTML = stylesheet;
+    root.appendChild(style);
+    
+    // Add a div to be the visible slide
+    var content = document.createElement("div");
+    content.className = "content";
+    content.setAttribute("aria-live", "assertive");
+    content.setAttribute("aria-atomic", true);
+    content.setAttribute("aria-relevant", "text");
+    root.appendChild(content);
+    
+    // deferred rendering
+    this.waitingToRender = null;
+    
+    // Let's make some state available and start up
+    this.index_ = this.getAttribute("index") * 1;
+    this.length = 0;
+    this.dom = { content, root };
+    this.scheduleRender();
+    this.dispatchEvent(new CustomEvent("slides-ready", {
+      detail: {
+        index: this.index_,
+        length: this.length
+      }
+    }));
+    
+    // Listen for updates from children
+    this.addEventListener("slide-content", e => this.scheduleRender());
+    var watcher = new MutationObserver(() => this.scheduleRender());
+    watcher.observe(this, { childList: true });
   }
-};
 
-// V1: this is "connectedCallback" instead
-// This is used in a more interesting way in slide-elements.js
-slideShowProto.attachedCallback = slideShowProto.connectedCallback = function() {
-  if (!this.upgraded) this.upgrade();
-};
+  // When attributes change, update the slideshow to match
+  // In V1, you must specify `observedAttributes` manually
+  static get observedAttributes() {
+    return ["index"]
+  }
 
-// V1: this becomes "disconnectedCallback"
-slideShowProto.detachedCallback = slideShowProto.disconnectedCallback = function() {};
-
-// Lots of things may trigger render, but defer it to a single update on the next frame
-slideShowProto.scheduleRender = function(index) {
-  if (typeof index != "undefined") this.index_ = index * 1;
-  if (this.waitingToRender) return;
-  this.waitingToRender = true;
-  window.requestAnimationFrame(() => this.render());
-}
-
-slideShowProto.render = function() {
-  this.waitingToRender = false;
-  // Find all slide children
-  var items = this.querySelectorAll("text-slide,code-slide");
-  // Update our internal state a bit
-  this.length = items.length;
-  if (this.index_ > items.length - 1) this.index_ = items.length - 1;
-  if (this.index_ < 0) this.index_ = 0;
-  // get the current slide
-  var selected = items[this.index_];
-  if (!selected) return;
-  // Load the slide's contents from participating elements
-  var slide = selected.parsedContent || {}
-  // Fill the content div with our new slide text
-  var content = this.dom.content;
-  content.innerHTML = `<h1>${slide.headline}</h1> ${slide.body}`;
-  content.firstElementChild.focus();
-  // Let listeners know that we've changed slides
-  this.dispatchEvent(new CustomEvent("slides-changed", {
-    detail: {
-      index: this.index_,
-      length: items.length
+  attributeChangedCallback(prop, before, after) {
+    switch (prop) {
+      case "index":
+        this.scheduleRender(after);
+        break;
+        
+      // TODO: other attributes go here
     }
-  }));
-};
+  }
 
-//boring convenience methods
-slideShowProto.getSlide = function() {
-  return this.index_;
-};
+  // V1: this is "connectedCallback" instead
+  // This is used in a more interesting way in slide-elements.js
+  attachedCallback() {
+    if (!this.upgraded) this.upgrade();
+  }
 
-//proxy index so that we can schedule render when it changes
-Object.defineProperty(slideShowProto, "index", {
-  enumerable: true,
-  get: function() {
+  connectedCallback() {
+    if (!this.upgraded) this.upgrade();
+  }
+
+  // Lots of things may trigger render, but defer it to a single update on the next frame
+  scheduleRender(index) {
+    if (typeof index != "undefined") this.index_ = index * 1;
+    if (this.waitingToRender) return;
+    this.waitingToRender = true;
+    window.requestAnimationFrame(() => this.render());
+  }
+
+  render() {
+    this.waitingToRender = false;
+    // Find all slide children
+    var items = this.querySelectorAll("text-slide,code-slide");
+    // Update our internal state a bit
+    this.length = items.length;
+    if (this.index_ > items.length - 1) this.index_ = items.length - 1;
+    if (this.index_ < 0) this.index_ = 0;
+    // get the current slide
+    var selected = items[this.index_];
+    if (!selected) return;
+    // Load the slide's contents from participating elements
+    var slide = selected.parsedContent || {}
+    // Fill the content div with our new slide text
+    var content = this.dom.content;
+    content.innerHTML = `<h1>${slide.headline}</h1> ${slide.body}`;
+    content.firstElementChild.focus();
+    // Let listeners know that we've changed slides
+    this.dispatchEvent(new CustomEvent("slides-changed", {
+      detail: {
+        index: this.index_,
+        length: items.length
+      }
+    }));
+  }
+
+  //boring convenience methods
+  getSlide() {
     return this.index_;
-  },
-  set: function(i) {
+  }
+
+  //proxy index so that we can schedule render when it changes
+  get index() {
+    return this.index_;
+  }
+  set index(i) {
     this.index_ = i;
     this.scheduleRender(i);
   }
-});
 
-slideShowProto.shiftSlide = function(delta) {
-  this.scheduleRender(this.index_ + delta);
-};
+  shiftSlide(delta) {
+    this.scheduleRender(this.index_ + delta);
+  }
 
-slideShowProto.nextSlide = function() {
-  this.shiftSlide(1);
-};
+  nextSlide() {
+    this.shiftSlide(1);
+  }
 
-slideShowProto.previousSlide = function() {
-  this.shiftSlide(-1);
-};
+  previousSlide() {
+    this.shiftSlide(-1);
+  }
+}
 
 // And now, the final registration:
-document.registerElement("slide-show", { prototype: slideShowProto });
+document.registerElement("slide-show", { prototype: SlideShowElement.prototype });
 // V1 version:
 // window.customElements.define("slide-show", SlideShowElement);
